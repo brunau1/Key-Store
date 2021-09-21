03c6060865f27e3a7711fd77c322f9da9ee7682a
bruno.santos.u4 <bruno.santos@u4crypto.com>
2021-09-21 19:11:31 -0300

    initial structure

---
+++ script.js
@@ -0,0 +1,68 @@
+const inputs = {
+    search: document.querySelector('#search')
+}
+
+const products = [
+  {
+    name: "Flower key",
+    category: "cute",
+    description:
+      "Chave com formato de flor, na cor dourado. Acompanha fechadura.",
+    image: "images/products/1.png",
+    price: 5.99,
+  },
+  {
+    name: "Latoya kantrele key",
+    category: "medieval",
+    description: "Chave artesanal. Detalhes em bronze. Acompanha fechadura.",
+    image: "images/products/2.png",
+    price: 49.99,
+  },
+  {
+    name: "Conjunto horse, bomb & men.",
+    category: "kit",
+    description:
+      "Três chaves com formatos diferentes em aço inox. Acompanha fechadura.",
+    image: "images/products/3.png",
+    price: 39.9,
+  },
+  {
+    name: "Conjunto medieval keys",
+    category: "kit, medieval",
+    description:
+      "Três chaves com formatos diferentes em bronze. Acompanha fechadura.",
+    image: "images/products/4.png",
+    price: 59.99,
+  },
+  {
+    name: "Templars order key",
+    category: "fantasy",
+    description:
+      "Chave com símbolo da ordem dos templários, na cor azul e prata. Acompanha fechadura.",
+    image: "images/products/5.png",
+    price: 9.99,
+  },
+  {
+    name: "Axe key",
+    category: "fantasy",
+    description:
+      "Chave com formato de machado, na cor dourado e prata, com detalhes e metal. Acompanha fechadura.",
+    image: "images/products/6.png",
+    price: 14.9,
+  },
+  {
+    name: "Heart key",
+    category: "love",
+    description: "Chave e cadeado com formato de coração.",
+    image: "images/products/7.png",
+    price: 29.90,
+  },
+  {
+    name: "Red love",
+    category: "love",
+    description: "Chave comum com cadeado vermelho em formato de coração.",
+    image: "images/products/8.png",
+    price: 11.99,
+  },
+];
+
